16f815c66894bb1a113f15df892dece8943c04f1
mariamiecho <maria.miechowiecka@gmail.com>
2025-01-02 16:08:47 +0100

    Zmodyfikowalam main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #git status
+#aby zapisać 2 korki:
+#1 git add[nazwa pliku] 
+#2 git commit + -m[wiadomość]
+#git log - historia commitów
+#git push origin main
+
+import time
+print(time.time())
+print("test")
